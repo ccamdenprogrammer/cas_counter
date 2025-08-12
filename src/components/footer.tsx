670704e74ec472e4922016bc5aad4cc342c0f1c5
ccamdenprogrammer <ccamdendev@gmail.com>
2025-08-11 23:54:58 -0500

    toll

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,18 @@ function Footer() {
   return (
     <footer className="footer py-10 text-center">
       <div className="text-sm md:text-base">
-         Live Data sourced from{' '}
+        Live Data sourced from{' '}
         <a
           href="https://data.techforpalestine.org/"
           className="text-gray-400 underline hover:text-gray-400"
         >
           techforpalestine.org
         </a>
+      </div>
+
+      {/* brief note about underestimation */}
+      <div className="mt-2 text-xs text-gray-500 max-w-xl mx-auto">
+        Reported figures are a confirmed subset of deaths and exclude many caused by starvation, disease, missing persons, and other war-related factors. actual toll is believed to be far higher.
       </div>
 
       {/* discreet favicon attribution */}
@@ -21,7 +26,6 @@ function Footer() {
           className="underline hover:no-underline"
         >
           Palestine icons created by Freepik - Flaticon
-          
         </a>
       </div>
     </footer>
